98f03fe547f1a1204e31d1a6d05fe1331ee0a553
Sahilmahalle <113128545+Sahilmahalle@users.noreply.github.com>
2023-06-24 08:32:41 +0530

    Week 1: Python for data science ,AI and ML

---
+++ week1.py
@@ -0,0 +1,45 @@
+import sys
+
+#printing hello word in python
+print("hello python")
+
+#know the python version
+print(sys.version)
+
+#writng a comment in python
+
+#see the error
+print("what is error")
+#frint("what is error")
+
+#data type of following 
+print(type(6/2))
+print(type(6//2))
+
+#addition
+print(43+60+16+41)
+
+#multiplication
+print(5*5)
+
+#division
+print(25/5)
+
+#integer division
+print(25//6)
+
+#hrs in 160 min
+print ("hrs in 160 min is :", 160/60)
+
+#stroe value in variable
+x=23
+x=x+1
+print("x=",x)
+
+#find the value of x
+x1=3+2*2
+print("x1=",x1)
+
+x2=(3+2)*2
+print("x2=",x2)
+      
